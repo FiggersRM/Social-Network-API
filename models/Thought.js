a0f81521c0a8d082ea0683a87d4259e9aea6cedc
FiggersRM <MitchFiggers@gmail.com>
2023-07-05 16:54:18 -0400

    Finished thought and reaction schemas

--- models/Thought.js
+++ models/Thought.js
@@ -1,26 +1,44 @@
 const { Schema, model } = require("mongoose");
-const dayjs = require('dayjs');
+const reactionSchema = require('./Reaction');
+const dayjs = require("dayjs");
 
 const thoughtSchema = new Schema(
-    {
-        thoughtText: {
-            type: String,
-            required: true,
-            minlength: 1,
-            maxlength: 280
-        },
-        createdAt: {
-            type: Date,
-            default: Date.now,
-            get: formatDate
-        },
-        username: {
-            type: String,
-            required: true
-        }
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      minlength: 1,
+      maxlength: 280,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: formatDate,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    reactions: [reactionSchema],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true,
     }
+  }
 );
 
 function formatDate(date) {
-    return dayjs(date).format('M/D/YYYY');
-}
+  return dayjs(date).format("M/D/YYYY");
+};
+
+thoughtSchema
+.virtuals('ractionCount')
+.get(function () {
+    return this.reactions.length;
+});
+
+const Thought = model('thought', thoughtSchema);
+
+module.exports = Thought;
